f45957d5f7e9c939455474ed5218ce4cc8ca9c71
devbwoh <bwoh@outlook.kr>
2024-01-29 03:40:21 +0900

    🔒️ Connection

---
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet18database/SupabaseConnectionExample.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.prof01.jet18database
+
+class SupabaseConnectionExample {
+//class SupabaseConnection {
+
+    // https://supabase.com/dashboard/projects
+    companion object {
+        const val URL = "https://본인의 URL.supabase.co"
+        const val KEY = "본인의 Key"
+    }
+}
